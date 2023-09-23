addfac42935905fa220d2ab09bec5e2829685ac2
Loic <loic.hamdi@gmail.com>
2023-09-22 23:37:01 -0500

    Web support

--- lib/widget/cached_media_controller.dart
+++ lib/widget/cached_media_controller.dart
@@ -1,8 +1,8 @@
 import 'dart:typed_data';
-
-import 'package:cached_media/widget/cached_media.dart';
+import 'package:cached_media/cached_media.dart';
 import 'package:cached_media/widget/functions/functions.dart';
 import 'package:get_storage/get_storage.dart';
+import 'dart:developer' as developer;
 
 class CachedMediaSnapshot {
   late DownloadStatus status;
@@ -34,8 +34,21 @@ class CachedMediaController {
       _snapshot.mimeType = cmi.mimeType;
       _snapshot.status = DownloadStatus.success;
       _onSnapshotChanged(_snapshot);
+      printSnapshot('Success');
     } else {
       _onSnapshotChanged(_snapshot..status = DownloadStatus.error);
+      printSnapshot('Error');
+    }
+  }
+
+  void printSnapshot(String? from) {
+    if (getShowLogs) {
+      developer.log('''
+🗣️  _onSnapshotChanged() - from: $from
+_snapshot.bytes: ${_snapshot.bytes}
+_snapshot.mimeType: ${_snapshot.mimeType}
+_snapshot.status: ${_snapshot.status}
+''', name: 'Cached Media package');
     }
   }
 }
